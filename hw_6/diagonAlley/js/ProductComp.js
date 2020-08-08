917cbf12fd64eeec70dc4f0b5bc208f11ad058b8
RandyMarsh1993 <dturanin@mail.ru>
2020-08-08 04:40:02 +0300

    hw 6 and hw 7

---
+++ hw_6/diagonAlley/js/ProductComp.js
@@ -0,0 +1,44 @@
+Vue.component('products', {
+	data() {
+		return {
+			catalogUrl: '/catalogData.json',
+			products: [],
+			filteredproducts: [],
+		}
+	},
+	methods: {
+		filterProducts(searchLine) {
+			const filterStr = new RegExp(searchLine, 'i');
+			this.filteredproducts = this.products.filter(product => filterStr.test(product.title));
+		}
+	},
+	mounted() {
+		this.$parent.getJson(`${API + this.catalogUrl}`)
+			.then(data => {
+				for (let product of data) {
+					this.products.push(product);
+					this.filteredproducts.push(product);
+				}
+			});
+	},
+	template: `<div class="content-wrapper products">
+					<product
+					v-for="product of filteredproducts"
+					:key="product.id"
+					:product="product"
+					></product>
+				</div>`
+				
+});
+
+Vue.component('product', {
+	props: ['product'],
+	template: `<div class="product-page">
+					<a href="#" class="product-page__title link">{{product.title}}</a>
+					<span class="product-page__price">{{product.price}}</span>
+					<a href="#" class="product-page__image"><img v-bind:src="product.img" alt="" width="250" class="catalog-pic"/></a>
+					<div class="buttons-wrapper">
+						<button class="toBasketBtn" @click="$parent.$parent.$refs.basket.addProduct(product)">Купить</button>
+					</div>
+			</div>`
+});
